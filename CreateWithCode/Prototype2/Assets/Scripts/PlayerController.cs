eff3884ba007123594fdb0fe73329ed5638c4577
Miles Darnell <153337@student.vansd.org>
2022-10-10 10:17:17 -0700

    Update PlayerController.cs

--- CreateWithCode/Prototype2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput; 
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        horizontalInput =  Input.GetAxis(Horizontal);
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 
     // Update is called once per frame
