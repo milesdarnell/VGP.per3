a5a1b8e46e94a8e81f404116b99b0d70f655a119
Miles Darnell <153337@student.vansd.org>
2022-10-19 10:28:40 -0700

    uh

--- CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,8 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[ ] animalPrefabs;
     public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +20,10 @@ public class SpawnManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.S))
         {
+            // Randomly generate animal index and spawn position
+            Vector3 spawnPosZ = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+            0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
             animalPrefabs[animalIndex].transform.rotation);
         }
