93025bcab4afe47fdc9bfe016599bc7fae9c60e9
Miles Darnell <153337@student.vansd.org>
2022-10-25 10:41:35 -0700

    vmnfvhk

--- CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
@@ -20,12 +20,15 @@ public class SpawnManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.S))
         {
-            // Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
-            0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
-            animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation)
     }
 }
