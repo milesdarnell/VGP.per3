3c0c2aef69e6e62d1515b2630a0cb8ba5bed9c38
Miles Darnell <153337@student.vansd.org>
2022-11-14 10:18:33 -0800

    nm

--- CreateWithCode/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ CreateWithCode/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -8,7 +8,7 @@ public class SpawnManagerX : MonoBehaviour
 
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
-    private float spawnPosY = 30;
+    private float spawnPosY = 0;
 
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
@@ -20,7 +20,7 @@ public class SpawnManagerX : MonoBehaviour
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
