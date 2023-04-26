fc5ecc570d83af303b8092fe8f1fdadcf6cd32d8
Miles Darnell <153337@student.vansd.org>
2023-04-26 10:41:48 -0700

    ngmml,j

--- CreateWithCode/Personal Project/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Personal Project/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 25.0f;
+    private float zBound = 6;
     private Rigidbody playerRb;
 
     // Start is called before the first frame update
@@ -33,10 +34,32 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("PowerUP"))
+        if(other.gameObject.CompareTag("PowerUp"))
             {
             Destroy(other.gameObject);
             }
+    }
+
+    private void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    private void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
     }
 
 }
