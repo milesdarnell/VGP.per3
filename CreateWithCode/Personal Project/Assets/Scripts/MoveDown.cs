9cf37fda515d0afd6b630a5844c83a4e9bdf057d
Miles Darnell <153337@student.vansd.org>
2023-05-03 10:40:31 -0700

    fkgfjh

--- CreateWithCode/Personal Project/Assets/Scripts/MoveDown.cs
+++ CreateWithCode/Personal Project/Assets/Scripts/MoveDown.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MoveDown : MonoBehaviour
 {
+    private float speed = 5.0f; 
+
+    private double zDestroy = -10.0;
+    private Rigidbody objectRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        objectRb.AddForce(Vector3.forward * -speed);
+
+        if(transform.position.z < zDestroy)
+        {
+            Destroy(gameObject);
+        }
     }
 }
